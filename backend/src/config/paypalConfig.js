d354586262eb8e8c54daba5029059a094f10ecb6
Jiyun Han <jiyun.han@tum.de>
2024-08-22 21:33:08 +0200

    delete credential info

--- backend/src/config/paypalConfig.js
+++ backend/src/config/paypalConfig.js
@@ -1,7 +1,7 @@
-const paypal = require('@paypal/checkout-server-sdk');
+const paypal = require("@paypal/checkout-server-sdk");
 
-const clientId = 'AV1dAJvdd-gFuK5PTKf9tuIE6hShd_-KSHd7KYgnxd3IosigAHqHErRAfiPSi4HblApB_LB_Qbh1g0GC';
-const clientSecret = 'EHaqArCQ6uCHChC9rnJ0JYUrdiFVv0Pc4U3cGSc0qhP_o5XX3bMGWxLlwlebVYju7StybKwfPtmFY8xu';
+const clientId = "";
+const clientSecret = "";
 
 const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 const client = new paypal.core.PayPalHttpClient(environment);
